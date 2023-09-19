b05711688781c5fa52206112daf4f639821b340c
gsackley <garrett.sackley@gmail.com>
2023-09-18 22:41:33 -0600

    Working way better now!

--- Wordle.py
+++ Wordle.py
@@ -8,58 +8,55 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
-# Selects random word
-randomWord = random.choice(FIVE_LETTER_WORDS)
-
-# Milestone 1:
-# This list is for each letter
-
-
-print(randomWord)
+random_word = random.choice(FIVE_LETTER_WORDS)
 
 
 def wordle():
-    def enter_action(s):
+    row = 0
+    col = 0
+
+    def enter_action(s):  
+        row = gw.get_current_row()
+        col = 0        
+        word = ""    
+        for x in range(N_COLS):
+            word += gw.get_square_letter(row,col)
+            col = col + 1
        
-        if s.lower() in FIVE_LETTER_WORDS:
-            nextLetter = []
-            chosenLetter = []
-            for index, letter in enumerate(randomWord):
-                nextLetter.append(letter.upper())
-                # gw.set_square_letter(N_COLS - 5, index, letter)
+        #gw.show_message(word)
 
-            for index, l in enumerate(s):
-                chosenLetter.append(l.upper())
-                # gw.set_square_letter(N_COLS - 5, index, l)
-            print(chosenLetter)
-            print(nextLetter)
-
-            # x = 0
-
-            for index, anyLetter in enumerate(nextLetter):
-                if chosenLetter[index] in nextLetter:
-                    print('yellow')
-                    gw.set_square_color(gw.get_current_row(), index, PRESENT_COLOR)
-                if anyLetter == chosenLetter[index]:
-                    print('green')
-                    gw.set_square_color(gw.get_current_row(), index, CORRECT_COLOR)
-
-            gw.set_current_row(gw.get_current_row() + 1)
+        if word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("That's a good word")
+            square = 0
+            for x in range(N_COLS):
+                letter = gw.get_square_letter(row,square)
+                if letter.lower() == random_word[square]:
+                    gw.set_square_color(row,square,"#66BB66")
+                elif letter.lower() in random_word:
+                    gw.set_square_color(row,square, "#CCBB66")
+                else:
+                    gw.set_square_color(row,square, "#999999")
+                square = square+1
+            gw.set_current_row(row+1)
+        elif (word[-1]) == " ":
+            gw.show_message("Not enough letters")
         else:
-            gw.show_message("Not in word list")
-
+            gw.show_message("Not in word list")    
+   
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-    # Milestone 1:
-    # Breaks word up
-    # for index, letter in enumerate(randomWord):
-    #     nextLetter.append(letter)
-    #     gw.set_square_letter(N_COLS - 5, index, letter)
+    gw.set_current_row(0)
 
+    # for x in random_word:
+    #     gw.set_square_letter(row,col,x)
+    #     col = col + 1
 
+   
+print(random_word)
 # Startup code
+
 if __name__ == "__main__":
     wordle()
